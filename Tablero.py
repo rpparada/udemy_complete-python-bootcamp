ed290c079f2553af6ea6e2ed5554fe7f11c21257
Rodrigo Parada <rpparada@gmail.com>
2017-12-23 00:20:38 -0300

    Seguir trabajando en juego
    Se cambio juego para manejar solo un mazo de cartas y se avanzo en la clase tablero

--- Tablero.py
+++ Tablero.py
@@ -8,38 +8,47 @@ Created on Sun Dec 17 23:23:16 2017
 # Reglas extraidas desde http://www.casino.es/blackjack/como-jugar-blackjack/
 
 from Crupier import Crupier
+from MazoCartas import MazoCartas
+from Jugador import Jugador
 
 class Tablero(object):
     
     numero_max_jugadores = 7    
     
-    def __init__(self, crupier = Crupier("Carlos"), monto_incial = 0):
+    def __init__(self):
         self.estado_juego = False
         self.lista_jugadores = []
-        self.lista_cartas_activas = []
-        self.lista_cartas_inactivas = []
-        self.turno_jugador
-        self.crupier = crupier
-        self.monto_inicial = monto_incial
+        self.turno_jugador = 0
+        self.crupier = None
+        self.monto_inicial = 0
     
     def agrega_jugador(self, jugador):
-        if self.lista_jugadores.count() < 7:
+        if len(self.lista_jugadores) < 7:
             self.lista_jugadores.append(jugador)
         else:
             print 'Numero Max de Jugadore es 7'
+            
+    def quitar_jugador(self, jugador):
+        pass
     
-    def iniciar_juego(self, numero_juegos_cartas):
-        if self.lista_jugadores.count() == 0:
+    def iniciar_juego(self, mazo, crupier):
+        if len(self.lista_jugadores) == 0:
             print 'Juego no puede comenzar, no hay jugadores'
         elif self.estado_juego:
-            print 'Actualmente ya hay un juego en proceso'
-        elif numero_juegos_cartas < 0:
-            print 'Numero de Juego de Cartas debe ser mayor que 0'
+            print 'Actualmente ya hay un juego en progreso'
         else:
             self.estado_juego = True
-            self.lista_cartas_activas = 
-            self.lista_cartas_inactivas[:] = []
+            self.mazo = mazo
+            self.crupier = crupier
             self.turno_jugador = 0
             self.monto_inicial = 1000
-    
+            
+
+tablero = Tablero()
+rodrigo = Jugador("Rodrigo", 100)
+juan = Jugador("Jugador", 200)
+tablero.agrega_jugador(rodrigo)
+tablero.agrega_jugador(juan)
+tablero.iniciar_juego(MazoCartas(),Crupier("Pedro"))
+print 'listo'
     
